8ce6ae4d58eaccd7bb871234ef5c55ca63f34306
ashleymavericks <singhanurag024@gmail.com>
2024-03-29 07:19:09 +0530

    feat: respond with 200 on successful connection

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    connection, address = server_socket.accept()
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    connection.send(response.encode())
+    connection.close()
 
 if __name__ == "__main__":
     main()
